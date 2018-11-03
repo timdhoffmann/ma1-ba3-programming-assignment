9fb9cc52927f5cbee47c2167e6ef86e65932bdc6
timdhoffmann <9947384+timdhoffmann@users.noreply.github.com>
2018-11-03 13:09:02 +0100

    Server waits for key press before exiting application.

--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -2,11 +2,20 @@
 
 namespace Server
 {
-    class Program
+    internal class Client
     {
-        static void Main(string[] args)
+    }
+
+    internal class Server
+    {
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
         }
     }
 }
