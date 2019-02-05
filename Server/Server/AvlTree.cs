8b2160223f6e8d0a3cbe1644f3cfca81d578484f
Tim <9947384+timdhoffmann@users.noreply.github.com>
2019-02-05 19:10:50 +0100

    In the process of implementing Insert() from Common.
    Trying simpler Insert() from McMillan next.

--- Server/Server/AvlTree.cs
+++ Server/Server/AvlTree.cs
@@ -2,70 +2,145 @@
 
 namespace Server
 {
-    internal class AvlTree
+    /// <summary>
+    /// A self-balancing binary search tree.
+    /// </summary>
+    /// <typeparam name="T"> The generic type. Must implement IComparable T. </typeparam>
+    internal class AvlTree<T> where T : IComparable<T>
     {
-        public AvlNode Root { get; private set; }
+        #region Properties
+        /// <summary>
+        /// The root of this tree.
+        /// </summary>
+        public AvlNode<T> Root { get; private set; }
 
+        /// <summary>
+        /// The number of nodes in this tree.
+        /// </summary>
+        public int Count { get; private set; }
+        #endregion
+
+        #region Constructors
         public AvlTree()
         {
             Root = null;
+            Count = 0;
+        }
+        #endregion
+
+        #region Public Methods
+        #region Insertion
+        /// <summary>
+        /// Inserts value into the tree as a new newNode.
+        /// </summary>
+        /// <param name="value"> The value to insert. </param>
+        public void Insert(T value)
+        {
+            Root = Insert(Root, new AvlNode<T>(value));
+        }
+        #endregion
+
+        #region Finding
+        public AvlNode<T> FindMin()
+        {
+            var current = Root;
+            while (current.LeftChild != null)
+            {
+                current = current.LeftChild;
+            }
+
+            return current;
+        }
+
+        public AvlNode<T> FindMax()
+        {
+            var current = Root;
+            while (current.RightChild != null)
+            {
+                current = current.RightChild;
+            }
+
+            return current;
+        }
+
+        //public User FindUserId(int key)
+        //{
+        //    var current = Root;
+
+        //    if (current.Value)
+        //    {
+        //    }
+        //}
+        #endregion
+        #endregion
+
+        #region Private Methods
+        /// <summary>
+        /// Inserts a new node underneath the given root and re-balances the tree.
+        /// </summary>
+        /// <param name="root"> The root node. </param>
+        /// <param name="newNode"> The new node to insert. </param>
+        /// <returns> The new root of the tree after potential re-balancing. </returns>
+        private AvlNode<T> Insert(AvlNode<T> root, AvlNode<T> newNode)
+        {
+            if (root == null)
+            {
+                root = newNode;
+                Count++;
+            }
+            else
+            {
+                root.ResetHeight();
+                int compareResult = root.Value.CompareTo(newNode.Value);
+                if (compareResult > 0)
+                {
+                    root.LeftChild = Insert(root.LeftChild, newNode);
+                }
+                else if (compareResult < 0)
+                {
+                    root.RightChild = Insert(root.RightChild, newNode);
+                }
+                else
+                {
+                    throw new ArgumentException("Trying to insert duplicate node.");
+                }
+            }
+
+            root = ReBalanceFrom(root);
+            return root;
+        }
+
+        private AvlNode<T> ReBalanceFrom(AvlNode<T> node)
+        {
+            // TODO: Implementation.
+            throw new NotImplementedException();
         }
 
         // TODO: Implement Insert().
-        //private AvlNode Insert(IComparable item, AvlNode node)
+        //private AvlNode Insert(IComparable item, AvlNode newNode)
         //{
-        //    if (node == null)
+        //    if (newNode == null)
         //    {
-        //        node = new AvlNode(item, null, null);
+        //        newNode = new AvlNode(item, null, null);
         //    }
-        //    else if (item.CompareTo(node.User) < 0)
+        //    else if (item.CompareTo(newNode.Value) < 0)
         //    {
-        //        node.LeftNode = Insert(item, node.LeftNode);
+        //        newNode.LeftChild = Insert(item, newNode.LeftChild);
 
-        //        if (height(node.LeftNode) - height(node.RightNode) == 2)
+        //        if (height(newNode.LeftChild) - height(newNode.RightChild) == 2)
         //        {
-        //            node = RotateWithLeftChild(node);
+        //            newNode = RotateWithLeftChild(newNode);
         //        }
         //        else
         //        {
-        //            node = DoubleWithLeftChild(node);
+        //            newNode = DoubleWithLeftChild(newNode);
         //        }
         //    }
-        //    else if (item.CompareTo(node.User) > 0)
+        //    else if (item.CompareTo(newNode.Value) > 0)
         //    {
         //        // TODO: Implement (s. p 267).
         //    }
         //}
-
-        public User FindMin()
-        {
-            var current = Root;
-            while (current.LeftNode != null)
-            {
-                current = current.LeftNode;
-            }
-
-            return current.User;
-        }
-
-        public User FindMax()
-        {
-            var current = Root;
-            while (current.RightNode != null)
-            {
-                current = current.RightNode;
-            }
-
-            return current.User;
-        }
-
-        public User FindUserId(int key)
-        {
-            var current = Root;
-
-            if (current.User)
-            {
-            }
-        }
+        #endregion
     }
 }
