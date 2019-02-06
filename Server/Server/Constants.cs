fb1c4763c2bf3d9cc04d845d4cc381b26845ee2d
Tim <9947384+timdhoffmann@users.noreply.github.com>
2019-02-06 17:29:14 +0100

    Introduced a class for constants.

---
+++ Server/Server/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace Server
+{
+    internal static class Constants
+    {
+        /// Client Commands.
+        // Exits the connection with the server.
+        public const string ExitCommand = "exit";
+    }
+}
